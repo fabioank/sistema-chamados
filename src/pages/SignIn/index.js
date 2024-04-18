06afc7bfa3a4100cc0674963798682cf69a61987
Fabio Kulka <fabiokulka.67@gmail.com>
2024-04-18 14:21:43 -0300

    Criada a pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,42 @@
+import './signin.css';
+import logo from '../../assets/logo.png'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function SignIn(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
-        <div>
-            <h1>Pagina Login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='Logo do sistema de chamados'></img>
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                    <input 
+                    type='text' 
+                    placeholder='email@email.com'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    ></input>
+
+                    <input 
+                    type='password' 
+                    placeholder='***********'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+
+                    <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to='/register'>Criar uma conta</Link>
+                
+            </div>
         </div>
     );
 }
