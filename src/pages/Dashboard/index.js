3f50c3395379420e658a4277658920697b742719
Fabio Kulka <fabiokulka.67@gmail.com>
2024-04-26 11:15:20 -0300

    Pagina Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,66 @@
 import { AuthContext } from "../../contexts/auth";
 import { useContext } from "react";
 import Header from "../../components/Header";
+import './dashboard.css';
+import Title from "../../components/Title";
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-export default function Dashboard(){
+export default function Dashboard() {
 
-    const { logOut} = useContext(AuthContext);
+    const { logOut } = useContext(AuthContext);
 
-     async function handleLogout(){
+    async function handleLogout() {
         await logOut();
     }
 
-    return(
+    return (
         <div>
+            <Header />
 
-            <Header/>
-            <h1>Default dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                <Link to="new" className="new">
+                <FiPlus color="#fff" size={25}/>
+                Novo Chamado
+                </Link>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope="col">Cliente</th>
+                            <th scope="col">Assunto</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Cadastrado em</th>
+                            <th scope="col">#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-label="Cliente">Mercado Esquina</td>
+                            <td data-label="Assunto">Suporte</td>
+                            <td data-label="Status">Em aberto</td>
+                            <td data-label="Cadastrado">26/04/2024</td>
+                            <td data-label="#">
+                                <button className="action" style={{backgroundColor: '#3585f6'}}>
+                                    <FiSearch color="#fff" size={17}/>
+                                </button>
+                                <button className="action" style={{backgroundColor: '#f6a935'}}>
+                                    <FiEdit2 color="#fff" size={17}/>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+                </>
+
+            </div>
         </div>
     );
 }
